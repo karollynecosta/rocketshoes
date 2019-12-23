d2c7308a8d5ff27f9af5715f42554c54e8c4d3c3
karollynecosta <karolcosta2@gmail.com>
2019-12-22 23:04:54 -0200

    config do Reactotron

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -24,6 +24,7 @@ import { ProductList } from './styles';
   }
 
   handleAddProduct = product => {
+    // transmite as ações ao redux
     const {dispatch} = this.props;
 
     dispatch({
--- src/store/modules/cart/reducer.js
+++ src/store/modules/cart/reducer.js
@@ -1,3 +1,8 @@
+/* Inicia o carrinho com state vazio para receber a action
+
+    Importa o produto e faz com que o Redux atualize os atualiza os components
+    que usam essa função   */
+
 export default function cart(state = [], action) {
   switch(action.type){
    case 'ADD_TO_CART':
